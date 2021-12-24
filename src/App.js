f2c817145687d0b28f63e65c352b1e429b8d7e48
kg-phantom <ksurbayan@gmail.com>
2021-12-24 11:52:12 -0800

    footer icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      
+      <main>
+
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
+
+function Footer() {
+    return (
+        <footer className='flex-row space-evenly px-max'>
+            <a href='https://www.linkedin.com/in/kyrsten-gabrielle-urbayan-aa062918b/'>
+                <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href='https://github.com/kg-phantom'>
+                <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href='https://stackoverflow.com/users/16181871/kg-phantom'>
+                <FontAwesomeIcon icon={faStackOverflow} />
+            </a>
+        </footer>
+    )
+}
+
+export default Footer;
