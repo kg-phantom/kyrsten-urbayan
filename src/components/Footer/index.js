950a196acc019f7571ab852bc1032b33400fc6e7
kg-phantom <ksurbayan@gmail.com>
2021-12-25 21:15:26 -0800

    Resume page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ import { faLinkedin, faGithub, faStackOverflow } from '@fortawesome/free-brands-
 function Footer() {
     return (
         <footer className='my-5 flex-row space-evenly px-max'>
-            <a href='https://www.linkedin.com/in/kyrsten-gabrielle-urbayan-aa062918b/'>
+            <a href='https://www.linkedin.com/in/kyrsten-gabrielle-urbayan-aa062918b/' target='_blank' rel='noreferrer'>
                 <FontAwesomeIcon icon={faLinkedin} />
             </a>
-            <a href='https://github.com/kg-phantom'>
+            <a href='https://github.com/kg-phantom' target='_blank' rel='noreferrer'>
                 <FontAwesomeIcon icon={faGithub} />
             </a>
-            <a href='https://stackoverflow.com/users/16181871/kg-phantom'>
+            <a href='https://stackoverflow.com/users/16181871/kg-phantom' target='_blank' rel='noreferrer'>
                 <FontAwesomeIcon icon={faStackOverflow} />
             </a>
         </footer>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,33 @@
 import React from "react";
+import resume from '../../assets/documents/Kyrsten-Urbayan-Technical-Resume.pdf';
 
 function Resume() {
+    const frontEnd = ['HTML5', 'CSS', 'JavaScript', 'Bootstrap', 'JQuery', 'AJAX', 'Responsive Design', 'React'];
+    const backEnd = ['Node.js', 'Express.js', 'MySQL','Sequelize', 'MongoDB', 'Mongoose'];
+
     return (
         <div>
             <h2 className='py-2'>Resume</h2>
+            <div>
+                <p>
+                    Download my<span> </span>
+                    <a href={resume} target='_blank' rel='noreferrer'>resume</a>!
+                </p>
+                <br />
+                <h3>Front-End Proficiencies</h3>
+                <ul>
+                    {frontEnd.map(skill => (
+                        <li key={skill}>{skill}</li>
+                    ))}
+                </ul>
+                <br />
+                <h3>Back-End Proficiencies</h3>
+                <ul>
+                    {backEnd.map(skill => (
+                        <li key={skill}>{skill}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
