91e62e7bff9695b7fad018ec0742fedef63d3a84
kg-phantom <ksurbayan@gmail.com>
2021-12-25 18:04:25 -0800

    About section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
+import Resume from './components/pages/Resume';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('About');
 
-  // function renderPage() {
-
-  // }
+  function renderPage() {
+    switch (currentPage) {
+      case 'About':
+        return <About></About>;
+      case 'Portfolio':
+        return <Portfolio></Portfolio>;
+      case 'Contact':
+        return <Contact></Contact>;
+      case 'Resume':
+        return <Resume></Resume>;
+      default:
+        return <About></About>;
+    }
+  }
 
   return (
     <div>
@@ -15,7 +30,9 @@ function App() {
       currentPage={currentPage}
       setCurrentPage={setCurrentPage}></Header>
       <main>
-
+        <div>
+          {renderPage()}
+        </div>
       </main>
       <Footer></Footer>
     </div>
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Portfolio() {
+    return (
+        <div>
+            <h2 className='py-2'>Portfolio</h2>
+        </div>
+    );
+}
+
+export default Portfolio;
