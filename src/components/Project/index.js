0e192f4e3320e83e235bd0cd0e667b1d1767344a
kg-phantom <ksurbayan@gmail.com>
2022-02-01 11:07:59 -0800

    project card animations

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,31 +1,40 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Project({ project }) {
-    return (
-        <article className="flex-row project-card card py-2" key={project.name}>
-            <img src={require(`../../assets/images/projects/${project.id}.png`)} alt={project.name} className='project-image' />
-            <div className='project-info'>
-                <a
-                className="project-title px-1"
-                href={project.url}
-                target="_blank"
-                rel="noreferrer">
-                    {project.name}
-                </a>
-                <a
-                className='project-github'
-                href={project.github}
-                target='_blank'
-                rel='noreferrer'>
-                    <FontAwesomeIcon icon={faGithub} />
-                </a>
-                <h3>{project.tech}</h3>
-            </div>
-            
-        </article>
-    );
+  return (
+    <article className="flex-row project-card card py-2" key={project.name}>
+      <div className="card-side front">
+        <img
+          src={require(`../../assets/images/projects/${project.id}.png`)}
+          alt={project.name}
+          className="project-image"
+        />
+      </div>
+      <div className="card-side back">
+        <div className="project-info">
+          <a
+            className="project-title px-1"
+            href={project.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {project.name}
+          </a>
+          <a
+            className="project-github"
+            href={project.github}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <h3>{project.tech}</h3>
+        </div>
+      </div>
+    </article>
+  );
 }
 
 export default Project;
