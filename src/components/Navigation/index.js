a662d0cd937ce72baf98766f0470ca8f543c541f
kg-phantom <ksurbayan@gmail.com>
2022-02-02 00:09:28 -0800

    start implementation of page scroll

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,28 +1,35 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 function Nav(props) {
-    const tabs = ['About', 'Portfolio', 'Contact', 'Resume'];
+  const tabs = ["About", "Portfolio", "Contact", "Resume"];
 
-    const { currentPage, setCurrentPage } = props;
+  const { currentPage, setCurrentPage } = props;
 
-    return (
-        <nav>
-            <ul className='flex-row space-around'>
-                {
-                    tabs.map(tab => (
-                        <li 
-                        className={`mx-2 px-1 ${
-                            currentPage === tab ? 'navActive' : 'underline'
-                        }`}
-                        key={tab}
-                        onClick={() => setCurrentPage(tab)}>
-                            {tab}
-                        </li>
-                    ))
-                }
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul className="flex-row space-around">
+        {tabs.map((tab) => (
+          <li
+            className={`mx-2 px-1 ${
+              currentPage === tab ? "navActive" : "underline"
+            }`}
+            key={tab}
+          >
+            <Link
+              className="link"
+              onClick={() => setCurrentPage(tab)}
+              to={tab.toLowerCase()}
+              spy={true}
+              smooth={true}
+            >
+              {tab}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
